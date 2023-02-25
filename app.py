4253c94a3d0ca1ca7c31aa1393fb716fdfd3b0ee
dewaayu27 <dwayuputri35@gmail.com>
2023-02-25 17:46:23 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, url_for, redirect,session,jsonify
+import pandas as pd
+
+app = Flask(__name__)
+app = Flask(__name__,template_folder='temp')
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+if __name__=='__main__':
+  app.run()
